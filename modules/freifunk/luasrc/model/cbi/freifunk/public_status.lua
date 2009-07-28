Hostle/openwrt-luci-multi-user

commit d962c289947f12000b3b6aa5c88280427bd04d4e

modules/freifunk: fix same problem
git-svn-id: http://svn.luci.subsignal.org/luci/trunk@5150 ab181a69-ba2e-0410-a84d-ff88ab4c47bc

modules/freifunk/luasrc/model/cbi/freifunk/public_status.lua
@@ -27,11 +27,11 @@ f:field(DummyValue, "_memtotal", translate("m_i_memory")).value =
 string.format("%.2f MB (%.0f%% %s, %.0f%% %s, %.0f%% %s)",
 	tonumber(memtotal) / 1024,
 	100 * memcached / memtotal,
-	translate("mem_cached") or "",
+	tostring(translate("mem_cached"), "")),
 	100 * membuffers / memtotal,
-	translate("mem_buffered") or "",
+	tostring(translate("mem_buffered", "")),
 	100 * memfree / memtotal,
-	translate("mem_free") or "")
+	tostring(translate("mem_free", ""))
 
 f:field(DummyValue, "_systime", translate("m_i_systemtime")).value =
 os.date("%c")

commit 1f345d3e3d24f5ea656a274b9fcd19e704b306de

modules/freifunk: fix syntax error introduced in r5150
git-svn-id: http://svn.luci.subsignal.org/luci/trunk@5160 ab181a69-ba2e-0410-a84d-ff88ab4c47bc

modules/freifunk/luasrc/model/cbi/freifunk/public_status.lua
@@ -27,7 +27,7 @@ f:field(DummyValue, "_memtotal", translate("m_i_memory")).value =
 string.format("%.2f MB (%.0f%% %s, %.0f%% %s, %.0f%% %s)",
 	tonumber(memtotal) / 1024,
 	100 * memcached / memtotal,
-	tostring(translate("mem_cached"), "")),
+	tostring(translate("mem_cached", "")),
 	100 * membuffers / memtotal,
 	tostring(translate("mem_buffered", "")),
 	100 * memfree / memtotal,