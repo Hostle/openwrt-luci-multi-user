086a60e2b7bfafff9cc2677bca18d73c6e792af6
Hostle <hostle19@gmail.com>
2015-03-13 11:17:15 -0400

    Update index.lua
    Added multi-user support

--- modules/luci-mod-admin-full/luasrc/controller/admin/index.lua
+++ modules/luci-mod-admin-full/luasrc/controller/admin/index.lua
@@ -1,20 +1,34 @@
 -- Copyright 2008 Steven Barth <steven@midlink.org>
 -- Licensed to the public under the Apache License 2.0.
 
+--[[ multi-user support added by Hostle 12/12/14 ]]--
+
 module("luci.controller.admin.index", package.seeall)
 
 function index()
+--## Multi User ##--
+local fs = require "nixio.fs"
+local valid_users = {}
+
+--## load system users into tbl ##--
+  if fs.stat("/usr/lib/lua/luci/users.lua") then
+    local usw = require "luci.users"
+    valid_users = usw.login()
+  else
+--## no multi user so root is only valid user ##--
+    valid_users = { "root" }
+  end
+
 	local root = node()
 	if not root.target then
 		root.target = alias("admin")
 		root.index = true
 	end
-
 	local page   = node("admin")
 	page.target  = firstchild()
 	page.title   = _("Administration")
 	page.order   = 10
-	page.sysauth = "root"
+	page.sysauth = valid_users
 	page.sysauth_authenticator = "htmlauth"
 	page.ucidata = true
 	page.index = true
