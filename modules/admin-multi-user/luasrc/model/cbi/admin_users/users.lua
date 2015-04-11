a50df0beacee5c915c3afe61b7541e6314d4bfc2
Hostle <hostle19@gmail.com>
2015-04-11 16:12:46 -0400

    Update users.lua
    added new users model

--- modules/admin-multi-user/luasrc/model/cbi/admin_users/users.lua
+++ modules/admin-multi-user/luasrc/model/cbi/admin_users/users.lua
@@ -20,14 +20,12 @@ $Id: users.lua 12/12/2014 by Hostle
 
 m = Map("users", translate("User Configuration"), translate("Add / Remove or Edit Users."))
 
-
+local fs = require "nixio.fs"
 local groups_path = "/etc/group"
 local groups = {"admin", "user"}
-local nw = require "luci.dispatcher"
 local usw = require "luci.users"
 
-local menu = {}
-local menu = nw.index_list(menu)
+local s, o
 
 m.on_after_commit = function()
 			usw.load_ui_user_file()
@@ -36,27 +34,75 @@ m.on_after_commit = function()
 			usw.del_users()			
 		    end
 
-s = m:section(TypedSection, "user", "")
+s = m:section(TypedSection, "user")
 s.anonymous = true
 s.addremove = true
+
+s:tab("user",  translate("User Settings"))
+s:tab("status",  translate("Status Menus"))
+s:tab("system",  translate("System Menus"))
+s:tab("network",  translate("Network Menus"))
 
 function s.parse(self, ...)
 	TypedSection.parse(self, ...)
 end
 
 
-name = s:option(Value, "name", translate("User Name"))
-
-user_group = s:option(ListValue, "user_group", translate("User Group"))
+name = s:taboption("user", Value, "name", translate("User Name"))
+user_group = s:taboption("user", ListValue, "user_group", translate("User Group"))
 for k, v in ipairs(groups) do
 	user_group:value(v)
 end
 
-s:option(Flag, "shell", translate("Allow SSH Access")).rmempty = false 
+o = s:taboption("user", ListValue, "shell", translate("SSH Access"))
+o.rmempty = false
+o:value("1", "Enabled")
+o:value("0", "Disabled")
+o.default = "Enabled"
 
-menu_items = s:option(MultiValue, "menu_items", translate("Hidden Menu Items"))
-for k, v in ipairs(menu) do
-	menu_items:value(v)
-end
+o = s:taboption("user", Flag, "status_menu", translate("Enable Status Menus"))
+o.rmempty = true
+o.disabled ="iptables routes syslog dmesg processes realtime" 
+o.enabled = "nil"
+
+status_subs = s:taboption("status", MultiValue, "status_subs", translate("Status Menus"))
+status_subs.rmempty = true
+status_subs:depends("status_menu", "nil")
+status_subs:value("iptables", "Firewall")
+status_subs:value("routes", "Routes")
+status_subs:value("syslog", "System Log") 
+status_subs:value("dmesg", "Kernel Log")
+status_subs:value("processes", "Processes")
+status_subs:value("realtime", "Realtime Graphs")
+
+o = s:taboption("user", Flag, "system_menu", translate("Enable System Menus"))
+o.rmempty = true 
+o.disabled = "system" 
+o.enabled = "admins"
+
+system_subs = s:taboption("system", MultiValue, "system_subs", translate("System Menus"))
+system_subs.rmempty = true
+system_subs:depends("system_menu", "admins")
+system_subs:value("packages", "Software")
+system_subs:value("startup", "Startup")
+system_subs:value("crontab", "Scheduled Tasks")
+system_subs:value("leds", "LED Configuration")
+system_subs:value("flashops", "Backup / Flash Firmware")
+system_subs:value("reboot", "Reboot")
+
+o = s:taboption("user", Flag, "network_menu", translate("Enable Network Menu"))
+o.rmempty = true 
+o.disabled ="network" 
+o.enabled = "nil"
+
+network_subs = s:taboption("network", MultiValue, "network_subs", translate("Network Menus"))
+network_subs.rmempty = true
+network_subs:depends("network_menu", "nil")
+network_subs:value("wireless", "Wifi")
+network_subs:value("dhcp", "DHCP and DNS")
+network_subs:value("hosts", "Hostnames")
+network_subs:value("netroutes", "Static Routes")
+network_subs:value("firewall", "FireWall")
+network_subs:value("diagnostics", "Diagnostics")
 
 return m
