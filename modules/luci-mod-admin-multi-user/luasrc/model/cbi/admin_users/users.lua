efaa6c29ebbd10ed69d7fa601052269d366ebac7
Hostle <hostle19@gmail.com>
2016-01-10 18:25:05 -0500

    Update users.lua
    I few minor adjustments

--- modules/luci-mod-admin-multi-user/luasrc/model/cbi/admin_users/users.lua
+++ modules/luci-mod-admin-multi-user/luasrc/model/cbi/admin_users/users.lua
@@ -1,15 +1,11 @@
 --[[
 LuCI - Lua Configuration Interface
-
 Copyright 2008 Steven Barth <steven@midlink.org>
 Copyright 2011 Jo-Philipp Wich <xm@subsignal.org>
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
 	http://www.apache.org/licenses/LICENSE-2.0
-
 $Id$
 ]]--
 
@@ -60,54 +56,51 @@ o:value("1", "Enabled")
 o:value("0", "Disabled")
 o.default = "Enabled"
 
-o = s:taboption("user", Flag, "status_menu", translate("Enable Status Menu"))
+o = s:taboption("user", Flag, "Status_menus", translate("Enable Status Menu"))
 o.rmempty = true
-o.disabled ="iptables routes syslog dmesg processes realtime" 
-o.enabled = "nil"
+o.disabled = "disabled" 
+o.enabled = "Status_menus"
 
 status_subs = s:taboption("status", MultiValue, "status_subs", translate("Status Menus"))
 status_subs.rmempty = true
-status_subs:depends("status_menu", "nil")
---status_subs:value("overview", "Overview")
-status_subs:value("iptables", "Firewall")
-status_subs:value("routes", "Routes")
-status_subs:value("syslog", "System Log") 
-status_subs:value("dmesg", "Kernel Log")
-status_subs:value("processes", "Processes")
-status_subs:value("realtime", "Realtime Graphs")
+status_subs:depends("Status_menus", "Status_menus")
+--status_subs:value("Overview", "Overview")
+status_subs:value("Firewall", "Firewall")
+status_subs:value("Routes", "Routes")
+status_subs:value("System_log", "System Log") 
+status_subs:value("Kernel_log", "Kernel Log")
+status_subs:value("Processes", "Processes")
+status_subs:value("Realtime_graphs", "Realtime Graphs")
 
-o = s:taboption("user", Flag, "system_menu", translate("Enable System Menu"))
+o = s:taboption("user", Flag, "System_menus", translate("Enable System Menu"))
 o.rmempty = true 
-o.disabled = "system" 
-o.enabled = "admins"
+o.disabled = "disabled" 
+o.enabled = "System_menus"
 
 system_subs = s:taboption("system", MultiValue, "system_subs", translate("System Menus"))
 system_subs.rmempty = true
-system_subs:depends("system_menu", "admins")
-system_subs:value("system_tab", "System")
---system_subs:value("admin", "Administration")
-system_subs:value("packages", "Software")
-system_subs:value("startup", "Startup")
-system_subs:value("crontab", "Scheduled Tasks")
-system_subs:value("leds", "LED Configuration")
-system_subs:value("flashops", "Backup / Flash Firmware")
-system_subs:value("reboot", "Reboot")
+system_subs:depends("System_menus", "System_menus")
+system_subs:value("System", "System")
+--system_subs:value("Administration", "Administration")
+system_subs:value("Software", "Software")
+system_subs:value("Startup", "Startup")
+system_subs:value("Scheduled_tasks", "Scheduled Tasks")
+system_subs:value("Leds", "LED Configuration")
+system_subs:value("Firmware", "Backup / Flash Firmware")
+system_subs:value("Reboot", "Reboot")
 
-o = s:taboption("user", Flag, "network_menu", translate("Enable Network Menu"))
+o = s:taboption("user", Flag, "Network_menus", translate("Enable Network Menu"))
 o.rmempty = true 
-o.disabled ="network" 
-o.enabled = "nil"
+o.disabled = "disabled" 
+o.enabled = "Network_menus"
 
 network_subs = s:taboption("network", MultiValue, "network_subs", translate("Network Menus"))
 network_subs.rmempty = true
-network_subs:depends("network_menu", "nil")
-network_subs:value("network_tab", "Interfaces")
-network_subs:value("wireless", "Wifi")
-network_subs:value("vlan", "Switch")
-network_subs:value("dhcp", "DHCP and DNS")
-network_subs:value("hosts", "Hostnames")
-network_subs:value("netroutes", "Static Routes")
-network_subs:value("firewall", "FireWall")
-network_subs:value("diagnostics", "Diagnostics")
+network_subs:depends("Network_menus", "Network_menus")
+network_subs:value("Interfaces", "Interfaces")
+network_subs:value("Wifi", "Wifi")
+network_subs:value("Switch", "Switch")
+network_subs:value("Dhcp", "DHCP and DNS")
+network_subs:value("Diagnostics", "Diagnostics")
 
 return m
